e794aafe246e1bef1605364570f6f7d2897e539c
Murilo Bastos <contato@murilobast.com>
2015-10-24 03:29:40 -0200

    Initial commit

---
+++ meteor/client/header/header.js
@@ -0,0 +1,10 @@
+Template.header.rendered = function () {
+	var $headLine = $('#headline');
+	$headLine.typed({
+		strings: ["Murilo Bastos"],
+		contentType: 'text',
+		typeSpeed: 200,
+		startDelay: 500,
+		showCursor: true
+	});
+};
---
+++ static/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	var $headLine = $('#headline');
+	$headLine.typed({
+		strings: ["Murilo Bastos"],
+		contentType: 'text',
+		typeSpeed: 200,
+		startDelay: 500,
+		showCursor: true
+	});
+});
